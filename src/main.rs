832e76ddbd2e3a02ff2115f971e448f6b552dccc
Jesse Vorisek <jesse.vorisek@gmail.com>
2022-01-08 11:21:37 -0800

    Lots of updates to notes files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+// TODO:
+// create an enum for terrain
+// hard-code a really simple map
+// use some sort of curses library to render the map
+// start handling user input
+//    q should exit
+//    add a player location and render an @
+//    arrow keys should move the @ around
+//    maybe disallow moving into walls
 fn main() {
     println!("Hello, world!");
 }
