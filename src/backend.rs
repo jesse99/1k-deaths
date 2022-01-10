ea2cb5fafc9c7b06e10f4b3615c76c68bf2c8914
Jesse Vorisek <jesse.vorisek@gmail.com>
2022-01-10 09:20:41 -0800

    More comments

--- src/backend.rs
+++ src/backend.rs
@@ -35,33 +35,33 @@ pub enum Topic {
     // /// An operation could not be completed.
     // Error,
     /// Something that doesn't affect the game, e.g. bumping into a wall.
-    NonGamePlay,
-    // /// NPC was damaged (but not by the player).
-    // NpcIsDamaged, // TODO: might want to have a separate Topic for player allies
+    NonGamePlay, // TODO: might want to change this to Announcement and add an ImportantAnnouncement
+                 // /// NPC was damaged (but not by the player).
+                 // NpcIsDamaged, // TODO: might want to have a separate Topic for player allies
 
-    // /// NPC was attacked but not damaged (but not by the player).
-    // NpcIsNotDamaged,
+                 // /// NPC was attacked but not damaged (but not by the player).
+                 // NpcIsNotDamaged,
 
-    // /// The player has caused damage.
-    // PlayerDidDamage,
+                 // /// The player has caused damage.
+                 // PlayerDidDamage,
 
-    // /// The player attacked but did no damage.
-    // PlayerDidNoDamage,
+                 // /// The player attacked but did no damage.
+                 // PlayerDidNoDamage,
 
-    // /// The player has taken damage.
-    // PlayerIsDamaged,
+                 // /// The player has taken damage.
+                 // PlayerIsDamaged,
 
-    // /// The player was attacked but took no damage.
-    // PlayerIsNotDamaged,
+                 // /// The player was attacked but took no damage.
+                 // PlayerIsNotDamaged,
 
-    // /// The player will operate less well.
-    // PlayerIsImpaired, // TODO: probably also want a PlayerEnchanced
+                 // /// The player will operate less well.
+                 // PlayerIsImpaired, // TODO: probably also want a PlayerEnchanced
 
-    // /// The player is at risk of taking damage.
-    // PlayerIsThreatened,
+                 // /// The player is at risk of taking damage.
+                 // PlayerIsThreatened,
 
-    // /// An operation was not completely successful.
-    // Warning,
+                 // /// An operation was not completely successful.
+                 // Warning,
 }
 
 #[derive(Clone, Eq, PartialEq)]
