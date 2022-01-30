ffaa4048b34bf09b0e4ec55dbb6cfbe18a1c52d0
Jesse Vorisek <jesse.vorisek@gmail.com>
2022-01-30 11:44:31 -0800

    WIP on supporting command line options

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,57 @@ mod backend;
 mod terminal;
 
 use backend::Game;
+use clap::{ArgEnum, Parser};
 use simplelog::{CombinedLogger, ConfigBuilder, LevelFilter, WriteLogger};
 use std::fs::File;
+use std::path::PathBuf;
+
+#[repr(usize)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, ArgEnum)]
+pub enum LoggingLevel {
+    // can't use simplelog::Level because it doesn't derive ArgEnum
+    Error,
+    Warn,
+    Info,
+    Debug,
+    Trace,
+}
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)] // TODO: could do better here but terminal support wil go away at some point
+struct Args {
+    /// ignore any saved files
+    #[clap(long)]
+    new_game: bool,
+
+    /// path to saved file
+    #[clap(long, parse(from_os_str), value_name = "PATH")]
+    load: Option<PathBuf>,
+
+    /// fixed random number seed (defaults to random)
+    #[clap(long, value_name = "N")]
+    seed: Option<u32>,
+
+    /// logging verbosity
+    #[clap(long, arg_enum, value_name = "NAME", default_value_t = LoggingLevel::Info)]
+    log_level: LoggingLevel,
+
+    /// enable special developer commands
+    #[clap(long)]
+    wizard: bool,
+}
 
 fn main() {
-    let config = ConfigBuilder::new()
+    let cli = Args::parse();
+
+    let logging = ConfigBuilder::new()
         .set_target_level(LevelFilter::Off)
         .set_thread_level(LevelFilter::Off)
         .set_location_level(LevelFilter::Off)
         .build();
     CombinedLogger::init(vec![WriteLogger::new(
         LevelFilter::Debug,
-        config,
+        logging,
         File::create("1k-deaths.log").unwrap(),
     )])
     .unwrap();
