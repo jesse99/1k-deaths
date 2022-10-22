f121f7478c021e1030feb4a2ed53ffa7012742b6
Jesse Vorisek <jesse.vorisek@gmail.com>
2022-10-22 12:07:47 -0700

    WIP on configuring logging options using command line args

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,48 @@
 extern crate log;
 extern crate simplelog;
 
+use clap::Parser;
 use simplelog::*;
+use std::{fs::File, str::FromStr};
 
-use std::fs::File;
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Can be trace, debug, info, warn, error, or off
+    #[arg(long, default_value = "info", value_name = "LEVEL")]
+    log_level: String,
+
+    /// Log file and line number at LEVEL and above
+    #[arg(long, default_value = "off", value_name = "LEVEL")]
+    log_location: String,
+
+    /// Relative path to log file
+    #[arg(long, default_value = "1k-deaths.log", value_name = "PATH")]
+    log_path: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
 
 // TODO:
 // add options to control the logger
-//  set_max_level
-//  location
-//  allow modules
-//  ignore modules
+//  support allow modules
+//  support ignore modules
 fn main() {
+    let args = Args::parse();
+    let log_level = LevelFilter::from_str(&args.log_level).expect("oops"); // TODO: don't use oops
+    let location = LevelFilter::from_str(&args.log_location).expect("oops");
+
     let config = ConfigBuilder::new()
         .set_thread_level(LevelFilter::Off) // don't log thread IDs
         .set_target_level(LevelFilter::Off) // don't log exe name
-        .set_location_level(LevelFilter::Off) // include file and line?
+        .set_location_level(location) // include file and line?
         .build();
-    let _ = WriteLogger::init(LevelFilter::Debug, config, File::create("1k-deaths.log").unwrap()).unwrap();
+    let _ = WriteLogger::init(log_level, config, File::create(&args.log_path).unwrap()).unwrap();
 
+    // TODO: at info log timestamp and version, maybe args too
     error!("error level");
-    info!("info level");
+    info!("logging to {}", args.log_path);
     debug!("debug level");
 }
