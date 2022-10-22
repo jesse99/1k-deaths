2243a8acdc4bc5f32b53fdcbc6bd02259585dfa6
Jesse Vorisek <jesse.vorisek@gmail.com>
2022-10-22 11:22:19 -0700

    Added support for logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+#[macro_use]
+extern crate log;
+extern crate simplelog;
+
+use simplelog::*;
+
+use std::fs::File;
+
+// TODO:
+// add options to control the logger
+//  set_max_level
+//  location
+//  allow modules
+//  ignore modules
 fn main() {
-    println!("Hello world!");
+    let config = ConfigBuilder::new()
+        .set_thread_level(LevelFilter::Off) // don't log thread IDs
+        .set_target_level(LevelFilter::Off) // don't log exe name
+        .set_location_level(LevelFilter::Off) // include file and line?
+        .build();
+    let _ = WriteLogger::init(LevelFilter::Debug, config, File::create("1k-deaths.log").unwrap()).unwrap();
+
+    error!("error level");
+    info!("info level");
+    debug!("debug level");
 }
