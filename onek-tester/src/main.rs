ceec5ab49a70800c7e08c63d5b86d9e3192025cd
Jesse Vorisek <jesse.vorisek@gmail.com>
2023-08-13 19:06:08 -0700

    Bit of renaming

--- onek-tester/src/main.rs
+++ onek-tester/src/main.rs
@@ -5,7 +5,7 @@ use onek_types::*;
 use std::time::Duration;
 
 trait ToSnapshot {
-    fn to_snapshot(&self, tx: &ipmpsc::Sender) -> String;
+    fn to_snapshot(&self, tx_state: &ipmpsc::Sender) -> String;
 }
 
 fn terrain_to_char(terrain: Terrain) -> char {
@@ -16,22 +16,22 @@ fn terrain_to_char(terrain: Terrain) -> char {
 }
 
 impl ToSnapshot for EditCount {
-    fn to_snapshot(&self, _tx: &ipmpsc::Sender) -> String {
+    fn to_snapshot(&self, _tx_state: &ipmpsc::Sender) -> String {
         format!("edit {self}")
     }
 }
 
 impl ToSnapshot for StateResponse {
-    fn to_snapshot(&self, tx: &ipmpsc::Sender) -> String {
+    fn to_snapshot(&self, tx_state: &ipmpsc::Sender) -> String {
         match self {
-            StateResponse::Map(map) => map.to_snapshot(tx),
-            StateResponse::Updated(count) => count.to_snapshot(tx),
+            StateResponse::Map(map) => map.to_snapshot(tx_state),
+            StateResponse::Updated(count) => count.to_snapshot(tx_state),
         }
     }
 }
 
 impl ToSnapshot for View {
-    fn to_snapshot(&self, _tx: &ipmpsc::Sender) -> String {
+    fn to_snapshot(&self, _tx_state: &ipmpsc::Sender) -> String {
         let mut result = String::with_capacity(200);
         for y in self.top_left.y..=self.top_left.y + self.size().height {
             for x in self.top_left.x..=self.top_left.x + self.size().width {
@@ -49,7 +49,7 @@ impl ToSnapshot for View {
             }
             result.push('\n');
         }
-        // At some point will need to use tx to include details about objects.
+        // At some point will need to use tx_state to include details about objects.
         result
     }
 }
@@ -63,35 +63,35 @@ fn state_sender() -> ipmpsc::Sender {
 }
 
 #[cfg(test)]
-fn state_receiver(tx: &ipmpsc::Sender) -> (String, ipmpsc::Receiver) {
+fn state_receiver(tx_state: &ipmpsc::Sender) -> (String, ipmpsc::Receiver) {
     let name = "/tmp/tester-sink";
-    let rx = match SharedRingBuffer::create(name, 32 * 1024) {
+    let rx_state = match SharedRingBuffer::create(name, 32 * 1024) {
         Ok(buffer) => Receiver::new(buffer),
         Err(err) => panic!("error opening tester-sink: {err:?}"),
     };
 
     let mesg = StateMessages::RegisterForQuery(ChannelName::new(name));
-    let result = tx.send(&mesg);
+    let result = tx_state.send(&mesg);
     assert!(!result.is_err(), "error sending RegisterForQuery to State: {result:?}");
 
-    (name.to_string(), rx)
+    (name.to_string(), rx_state)
 }
 
 #[cfg(test)]
-fn send_reset(tx: &ipmpsc::Sender, map: &str) {
+fn send_reset(tx_state: &ipmpsc::Sender, map: &str) {
     let mesg = StateMessages::Mutate(StateMutators::Reset(map.to_string()));
-    let result = tx.send(&mesg);
+    let result = tx_state.send(&mesg);
     assert!(!result.is_err(), "error sending to State: {result:?}")
 }
 
 #[cfg(test)]
-fn get_player_view(tx: &ipmpsc::Sender, rx: &ipmpsc::Receiver, name: &str) -> StateResponse {
+fn get_player_view(tx_state: &ipmpsc::Sender, rx_state: &ipmpsc::Receiver, name: &str) -> StateResponse {
     let name = ChannelName::new(name);
     let mesg = StateMessages::Query(StateQueries::PlayerView(name));
-    let result = tx.send(&mesg);
+    let result = tx_state.send(&mesg);
     assert!(!result.is_err(), "error sending to State: {result:?}");
 
-    let result = rx.recv_timeout(Duration::from_millis(100));
+    let result = rx_state.recv_timeout(Duration::from_millis(100));
     assert!(!result.is_err(), "error receiving from State: {result:?}");
 
     let option = result.unwrap();
@@ -102,17 +102,17 @@ fn get_player_view(tx: &ipmpsc::Sender, rx: &ipmpsc::Receiver, name: &str) -> St
 
 #[test]
 fn test_from_str() {
-    let tx = state_sender();
+    let tx_state = state_sender();
     send_reset(
-        &tx,
+        &tx_state,
         "###\n\
              #@#\n\
              ###",
     );
 
-    let (rx_name, rx) = state_receiver(&tx);
-    let state = get_player_view(&tx, &rx, &rx_name);
-    insta::assert_display_snapshot!(state.to_snapshot(&tx));
+    let (rx_name, rx_state) = state_receiver(&tx_state);
+    let state = get_player_view(&tx_state, &rx_state, &rx_name);
+    insta::assert_display_snapshot!(state.to_snapshot(&tx_state));
 }
 
 fn main() {
