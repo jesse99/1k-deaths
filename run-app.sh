2b73d2c4b00ed7f3c394ce2a729b5f3b59ddb7b7
Jesse Vorisek <jesse.vorisek@gmail.com>
2024-01-26 09:08:17 -0800

    Added

---
+++ run-app.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# killall will send a signal to the process and then return.
+# When the signal is eventually delivered a message is written to stdout.
+# This is annoying but adding this trap handler suppresses that.
+trap 'exit 0' TERM
+
+# bash gets weird when trying to do `command& &&`
+run_backend() {
+	./target/debug/onek-backend&
+    sleep 1
+}
+
+run_terminal() {
+    ./target/debug/onek-terminal
+}
+
+cargo build && run_backend && run_terminal
+killall -q onek-backend
