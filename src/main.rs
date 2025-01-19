141050b70c1739e39221a3216be7de43fddeeb93
Jesse Vorisek <jesse.vorisek@gmail.com>
2025-01-19 14:06:01 -0800

    Use crossterm to write out hello world

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use crossterm::{
+    cursor,
+    style::{self, Stylize},
+    terminal, ExecutableCommand, QueueableCommand,
+};
+use std::io::{self, Write};
+fn main() -> io::Result<()> {
+    let mut stdout = io::stdout();
+
+    // TODO think we want raw mode, see https://docs.rs/crossterm/latest/crossterm/terminal/index.html#raw-mode
+    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+
+    let prompt = "hello world";
+    let (width, height) = terminal::size().unwrap();
+    let x = width / 2 - (prompt.bytes().len() / 2) as u16;
+    let y = height / 2;
+
+    stdout
+        .queue(cursor::MoveTo(x, y))?
+        .queue(style::PrintStyledContent(prompt.red()))?;
+    stdout.flush()?;
+    Ok(())
+}
